04a08f8cd60f97e8497fe464f52f350188d06b58
Andre <andrezaocordeiro@gmail.com>
2026-04-04 19:04:40 -0300

    add compare program and utility functions; remove unused hello.asm and its makefile

--- compare/compare.asm
+++ compare/compare.asm
@@ -1,4 +1,3 @@
-extern saida
 
 section .data
     msg_maior_de_idade db 'Maior de idade',0xA,0
@@ -8,6 +7,7 @@ section .data
     pergunta db 'Qual sua idade?',0xA,0
     len_pergunta equ $-pergunta
 
+; Variáveis que são armazenadas em runtime, ou seja, durante a execução do programa
 section .bss
     user_input resb 4
 
@@ -27,12 +27,15 @@ _start:
     mov edx, 4
     int 0x80
 
-    mov ebx, 18
-    cmp ebx, [user_input]
+    mov esi, user_input
+    mov edx, 4
+    call string_to_int
+
+    mov esi, 18
+    cmp eax, esi
+    
     jge maior_de_idade
     jl menor_de_idade
-    call saida
-
 
 maior_de_idade:
     mov eax, 4
@@ -41,9 +44,46 @@ maior_de_idade:
     mov edx, len_msg_maior_de_idade
     int 0x80
 
+    mov eax, 1      ; syscall exit
+    xor ebx, ebx
+    int 0x80
+
 menor_de_idade:
     mov eax, 4
     mov ebx, 1
     mov ecx, msg_menor_de_idade
     mov edx, len_msg_menor_de_idade
     int 0x80
+
+    mov eax, 1      ; syscall exit
+    xor ebx, ebx
+    int 0x80
+
+; ESI = STRING
+limpar_input:
+    
+
+; ESI = STRING INPUT
+; EDX = TAMANHO STRING
+; EAX = RESULTADO
+string_to_int:
+    xor ecx, ecx
+    xor ebx, ebx
+
+.next_digit:
+    movzx eax, byte [esi]
+    cmp al, '0'
+    jl .exit
+    cmp al, '9'
+    jg .exit
+    inc esi
+    sub eax, '0'
+    imul ebx, 10
+    add ebx, eax
+    inc ecx
+    cmp ecx, edx
+    jl .next_digit
+
+.exit:
+    mov eax, ebx
+    ret
